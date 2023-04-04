f914d890d0e6390c2c5eb8cf1276e300cd2c3042
Keely Timms <keely.a.timms@gmail.com>
2023-04-04 14:29:09 -0500

    Add calendar to org home

--- client/components/OrgCalendar.tsx
+++ client/components/OrgCalendar.tsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import styles from './OrgCalendar.module.css';
+
 const OrgCalendar = () => {
+  const initialDays: Date[] = [];
+  const [days, setDays] = useState<Date[] | undefined>(initialDays);
+
   return (
-    <div>
+    <div className={styles.container}>
+      <style>{`
+        .rdp {
+          --rdp-cell-size: 32px;
+        }
+        .rdp-months {
+          justify-content: center;
+        }
+        .rdp-day_selected, .rdp-day_selected:focus-visible, .rdp-day_selected:hover {
+          background-color: white;
+          color: var(--color-secondary);
+        }
+      `}</style>
+      <DayPicker
+        mode="multiple"
+        selected={days}
+        onSelect={setDays}
+      />
+      
     </div>
   );
 }
