ca1b4dfb84445c493a8a37406f409e3b3bcbd980
iancdavis <iancdavis7@gmail.com>
2023-04-04 13:48:59 -0600

    server connected to client

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,15 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/api/data', (req, res) => {
+  const data = { message: 'Hello from the backend!' };
+  res.json(data);
+});
 
 // Handle all other requests with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        autoRewrite: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 });
