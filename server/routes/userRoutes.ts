8de769a19e329957bc07ced0a051bec8da2d7e55
zacharydfreeman <zdf2424@gmail.com>
2023-04-04 19:09:10 -0500

    prettier formatting

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,25 +1,18 @@
-import express from "express";
-import userController from "../controllers/userController.js";
+import express from 'express';
+import userController from '../controllers/userController.js';
 
-const router = express.Router()
+const router = express.Router();
 
 //update user preferences (probably combine skill and pref into one)
-router.put('/updatePref', userController.updatePreferences, (req, res) => {
-});
+router.put('/updatePref', userController.updatePreferences, (req, res) => {});
 
 //update user skills
-router.put('/updateSkills', userController.updateskills, (req, res) => {
-
-});
+router.put('/updateSkills', userController.updateskills, (req, res) => {});
 
 //signup
-router.post('/signup', userController.signUp, (req, res) => {
-    
-});
+router.post('/signup', userController.signUp, (req, res) => {});
 
 //signin
-router.post('/login', userController.login, (req, res) => {
-
-});
+router.post('/login', userController.login, (req, res) => {});
 
 export default router;
