f45a3ce0ab33f3e79b371c2851231d413719de6b
zacharydfreeman <zdf2424@gmail.com>
2023-04-04 21:02:38 -0500

    updated user controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,25 +1,65 @@
-import { Request, Response, NextFunction } from 'express';
+import { UserModel } from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
 
 const userController: Record<string, any> = {};
-
-// login
-// handle auth here
-userController.login = async (req, res, next) => {};
 
 // signup
 // handle auth here
 userController.signUp = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const {
+      firstName,
+      lastName,
+      username,
+      password,
+      skills,
+      preferences,
+      eventRanks,
+    } = req.body;
+    const newUser = new UserModel({
+      firstName,
+      lastName,
+      username,
+      password,
+      skills,
+      preferences,
+      eventRanks,
+    });
+    const savedNewUser = await newUser.save();
+    res.locals.user = savedNewUser;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
 
+// login
+// handle auth here
+userController.login = async (req, res, next) => {
+  try {
+    console.log('Login req body: ', req.body);
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+    console.log(user);
+    bcrypt.compare(password, user?.password, (error, isMatch) => {
+      if (error) {
+        return next(error);
+      } else if (!isMatch) {
+        res.status(401).json('Login failed');
+      } else {
+        console.log('login success');
+        res.locals.user = user;
+      }
+      return next();
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 // update preferences
-userController.updatePreferences = async (req, res, next) => {};
-
-// update skills
-userController.updateskills = async (req, res, next) => {};
-
-// add skill
-userController.addskills = async (req, res, next) => {};
+userController.updateUser = async (req, res, next) => {};
 
 // delete user
 userController.deleteUser = async (req, res, next) => {};
