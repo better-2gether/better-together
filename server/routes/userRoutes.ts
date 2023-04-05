e7295c111c5b3062a0dc8f5fc389b9119a642de2
zacharydfreeman <zdf2424@gmail.com>
2023-04-05 00:52:52 -0500

    user basic routing completed

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -14,9 +14,13 @@ router.post('/login', userController.login, (req, res) => {
 });
 
 //update user preferences (probably combine skill and pref into one)
-router.put('/updateUser', userController.updateUser, (req, res) => {});
+router.patch('/updateUser', userController.updateUser, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
 
 // delete user
-router.delete('/deleteUser', userController.deleteUser, (req, res) => {});
+router.delete('/deleteUser', userController.deleteUser, (req, res) => {
+  res.status(200).json('User Deleted');
+});
 
 export default router;
