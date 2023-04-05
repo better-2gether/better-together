40f91ffdc8e2a1fce54b27bf84983ea91ee2f83c
zacharydfreeman <zdf2424@gmail.com>
2023-04-05 09:43:47 -0500

    added ranking logic to user controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,5 +1,8 @@
 /* eslint-disable */
 import { UserModel } from '../models/UserModel.js';
+import { OrgModel } from '../models/OrgModel.js';
+import { getUserEventRanks } from '../models/matchingAlgorithm.js';
+
 import bcrypt from 'bcryptjs';
 
 const userController: Record<string, any> = {};
@@ -58,6 +61,22 @@ userController.deleteUser = async (req, res, next) => {
   try {
     const { _id: userID } = req.body;
     const result = await UserModel.deleteOne({ _id: userID });
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+userController.getEventRanks = async (req, res, next) => {
+  try {
+    // need to get all orgs
+    const orgs = await OrgModel.find().populate({
+      path: 'events',
+      model: 'Event',
+    });
+    // pass user and orgs through ranking algorithm
+    const eventRanks = getUserEventRanks(res.locals.user, orgs);
+    res.locals.eventRanks = eventRanks;
     return next();
   } catch (error) {
     return next(error);
