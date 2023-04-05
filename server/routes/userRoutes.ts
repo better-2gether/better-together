933a2136822867e89128e7842f7bf1dcadee37d5
zacharydfreeman <zdf2424@gmail.com>
2023-04-05 09:44:27 -0500

    add ranking logic to middleware

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -4,19 +4,40 @@ import userController from '../controllers/userController.js';
 const router = express.Router();
 
 //signup
-router.post('/signup', userController.signUp, (req, res) => {
-  res.status(200).json(res.locals.user);
-});
+router.post(
+  '/signup',
+  userController.signUp,
+  userController.getEventRanks,
+  (req, res) => {
+    const user = res.locals.user;
+    user.eventRanks = res.locals.eventRanks;
+    res.status(200).json({ user, eventRanks: res.locals.eventRanks });
+  }
+);
 
 //signin
-router.post('/login', userController.login, (req, res) => {
-  res.status(200).json(res.locals.user);
-});
+router.post(
+  '/login',
+  userController.login,
+  userController.getEventRanks,
+  (req, res) => {
+    const user = res.locals.user;
+    user.eventRanks = res.locals.eventRanks;
+    res.status(200).json({ user, eventRanks: res.locals.eventRanks });
+  }
+);
 
 //update user preferences (probably combine skill and pref into one)
-router.patch('/updateUser', userController.updateUser, (req, res) => {
-  res.status(200).json(res.locals.user);
-});
+router.patch(
+  '/updateUser',
+  userController.updateUser,
+  userController.getEventRanks,
+  (req, res) => {
+    const user = res.locals.user;
+    user.eventRanks = res.locals.eventRanks;
+    res.status(200).json({ user, eventRanks: res.locals.eventRanks });
+  }
+);
 
 // delete user
 router.delete('/deleteUser', userController.deleteUser, (req, res) => {
