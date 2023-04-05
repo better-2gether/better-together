5ecd5bf6cfe5f235f77d90754bf372c3b9232ed4
Keely Timms <keely.a.timms@gmail.com>
2023-04-05 12:07:01 -0500

    WIP login page

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
 import OrgHome from './pages/OrgHome';
 import AddEvent from './pages/AddEvent';
 import PageNotFound from './pages/PageNotFound';
 import type { Org, User, Event } from './types';
 import './App.css';
 
-function App() {
-  const [user, setUser] = useState<Org | User>(sampleOrg);
-  const [isUser, setIsUser] = useState(false);
+function PrivateRoute({ user, children }) {
+  return user ? children : <Navigate replace to='/login' />;
+}
 
-  const [message, setMessage] = useState('');
+function PublicRoute({ user, children }) {
+  return user ? <Navigate replace to='/' /> : children;
+}
+
+function App() {
+  const [user, setUser] = useState<Org | User | null>(sampleOrg);
+  const [isUser, setIsUser] = useState(false);
 
   const updateOrgEvents = (events: Event[]) => {
     if (!isUser) setUser({ ...user, events });
@@ -23,12 +30,28 @@ function App() {
         <Navbar isUser={isUser} user={user} />
         <Routes>
           <Route
+            path='/login'
+            element={
+              <PublicRoute user={user}>
+                <Login setUser={setUser} setIsUser={setIsUser} />
+              </PublicRoute>
+            }
+          />
+          <Route
             path='/'
-            element={!isUser ? <OrgHome events={(user as Org).events} /> : <UserHome />}
+            element={
+              <PrivateRoute user={user}>
+                {!isUser ? <OrgHome events={(user as Org).events} /> : <UserHome />}
+              </PrivateRoute>
+            }
           />
           <Route
             path='/event'
-            element={!isUser && <AddEvent _id={user._id} updateOrgEvents={updateOrgEvents} />}
+            element={
+              <PrivateRoute user={user}>
+                {!isUser && <AddEvent _id={user._id} updateOrgEvents={updateOrgEvents} />}
+              </PrivateRoute>
+            }
           />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,4 +1,11 @@
-const Login = (): JSX.Element => {
+import type { Org, User } from '../types';
+interface LoginProps {
+  setUser: (user: User | Org) => void;
+  setIsUser: (isUser: boolean) => void;
+}
+
+const Login = (props: LoginProps): JSX.Element => {
+  const { setUser, setIsUser } = props;
   return <div>Login</div>;
 };
 
