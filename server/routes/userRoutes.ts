76d0dbda76cbcd3a9e027f111ee98b7560b7fb14
zacharydfreeman <zdf2424@gmail.com>
2023-04-04 21:01:29 -0500

    begun adding user routes Co-authored-by: Ian Davis <iancdavis@users.noreply.github.com>

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -3,16 +3,20 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 
-//update user preferences (probably combine skill and pref into one)
-router.put('/updatePref', userController.updatePreferences, (req, res) => {});
-
-//update user skills
-router.put('/updateSkills', userController.updateskills, (req, res) => {});
-
 //signup
-router.post('/signup', userController.signUp, (req, res) => {});
+router.post('/signup', userController.signUp, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
 
 //signin
-router.post('/login', userController.login, (req, res) => {});
+router.post('/login', userController.login, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
+
+//update user preferences (probably combine skill and pref into one)
+router.put('/updateUser', userController.updateUser, (req, res) => {});
+
+// delete user
+router.delete('/deleteUser', userController.deleteUser, (req, res) => {});
 
 export default router;
