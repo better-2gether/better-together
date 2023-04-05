9f9c174da74f6790ca3bdbaca405ca0c6e536cbc
zacharydfreeman <zdf2424@gmail.com>
2023-04-05 01:00:28 -0500

    basic org routing complete

--- server/routes/orgRoutes.ts
+++ server/routes/orgRoutes.ts
@@ -3,12 +3,24 @@ import orgController from '../controllers/orgController.js';
 
 const router = express.Router();
 
-router.post('/signup', orgController.signUp, (req, res) => {});
+router.post('/signup', orgController.signUp, (req, res) => {
+  res.status(200).json(res.locals.org);
+});
 
-router.post('/login', orgController.login, (req, res) => {});
+router.post('/login', orgController.login, (req, res) => {
+  res.status(200).json(res.locals.org);
+});
 
-router.post('/addEvent', orgController.addEvent, (req, res) => {});
+router.patch('/updateOrg', orgController.updateOrg, (req, res) => {
+  res.status(200).json(res.locals.org);
+});
 
-router.put('/updateCauses', orgController.updateOrg, (req, res) => {});
+router.post('/createEvent', orgController.createEvent, (req, res) => {
+  res.status(200).json(res.locals.org);
+});
+
+router.patch('/editEvent', orgController.editEvent, (req, res) => {
+  res.status(200).json(res.locals.org);
+});
 
 export default router;
